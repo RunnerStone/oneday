5b3c7e8fee3fc856f858d7185141331107f02add
leixu <leixu@192.168.31.101>
2018-03-20 21:48:36 +0800

    [new]

--- src/main/java/com/oneday/controller/DefaultController.java
+++ src/main/java/com/oneday/controller/DefaultController.java
@@ -1,6 +1,7 @@
 package com.oneday.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DefaultController {
 
-    @GetMapping
-    public String get() {
+	@GetMapping
+	public String get() {
 
-        return "ok";
-    }
+		return "ok";
+	}
+
+	@GetMapping("/right/{value}")
+	public Boolean isPalindrome(@PathVariable("value") int param) {
+		// if (param < 0 || param % 10 == 0 || param ==0) {
+		// return false;
+		// }
+		// int revertedNumber = 0;
+		// while (param > revertedNumber) {
+		// revertedNumber = revertedNumber * 10 + param % 10;
+		// param /= 10;
+		// }
+		// return param == revertedNumber || param == revertedNumber / 10;
+		// }
+		int x, y;
+		x=0;
+		y = param;
+		while (param > 0) {
+			x = param % 10 + x * 10;
+			param = param / 10;
+		}
+		if(y==x) {
+			return true;
+		}
+
+		return false;
+	}
+
 }
